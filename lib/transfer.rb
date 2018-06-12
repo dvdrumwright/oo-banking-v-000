26419d11c2401b097f6273a8dde8b0a7b755794f
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-12 22:14:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,8 +25,8 @@ end
 
 def reverse_transfer  
 if @status == "complete"
-  self.balance -= @amount
-  self.receiver += @amount
+  @sender.balance -= @amount
+  @receiver.balance += @amount
 else @status == "reversed"
 end
 end 
